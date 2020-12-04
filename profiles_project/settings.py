058329ba41f91808dc618e5d3a8b8d8d4fd81b94
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-12-04 13:10:14 +0200

    Adding SQLlite3 for testing

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,15 +78,16 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
 
+
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -95,7 +97,7 @@ DATABASES = {
         'HOST': 'localhost',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
